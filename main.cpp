c3f325163c5dbb60000c987a490eda40750cd424
g-ferz <s5005a@gmail.com>
2012-02-22 20:01:08 +0300

    modiff

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
      Q_INIT_RESOURCE(textedit);
 
      QApplication a( argc, argv );
-     TextEdit mw;
-     mw.resize( 700, 500 );
-     mw.show();
+     TextEdit mw1;
+     mw1.resize( 700, 500 );
+     mw1.show();
+
      return a.exec();
  }
