a4e348355f97883f3e2b34d3a918b5ddcb46457a
KosteleiAndrei <kostelei5392@mail.ru>
2012-02-28 19:37:51 +0300

    Commit kostelei

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@
  {
      Q_INIT_RESOURCE(textedit);
      QApplication a( argc, argv );
-     TextEdit mw;
-     mw.resize( 700, 500 );
-     mw.show();
-
+     TextEdit MyWindows;
+     MyWindows.resize( 700, 500 );
+     MyWindows.show();
      return a.exec();
  }
